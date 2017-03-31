358bf0d2208094b113bf4fe5f833de7ceee84097
root <chio@nzgft.com>
2017-03-31 11:37:50 +0800

    update

--- install_server.sh
+++ install_server.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 here=`pwd`
+mkdir /opt
 mkdir /opt/bin
-cd /opt/bin
+echo "------------------------ now downlaod kube-apiserver ------------------------"
 curl -O  https://storage.googleapis.com/kubernetes-release/release/v1.6.0/bin/linux/amd64/kube-apiserver
+echo "------------------------ now downlaod kube-controller-manager ------------------------"
 curl -O  https://storage.googleapis.com/kubernetes-release/release/v1.6.0/bin/linux/amd64/kube-controller-manager
+chmod +x kube-apiserver
+chmod +x kube-controller-manager
+mv kube-apiserver /opt/bin
+mv kube-controller-manager /opt/bin
 
-chmod +x *
+echo "----------------------------------- check downfile file !! -----------------------------"
+ls -l /opt/bin
 
-echo "check downfile file !!"
-ls -l
+read -n 1 -p "Press any key to continue..."
 
-pause 
 echo "============== copy config file ================="
 cd $here
 cp service/apiserver.service /etc/systemd/system/apiserver.service
@@ -20,13 +25,12 @@ echo "=================================================="
 systemctl daemon-reload
 systemctl start apiserver.service
 systemctl enable apiserver.service 
-sleep 20
 systemctl status apiserver.service
-netstat -natp |grep 8080
-pause
+netstat -natp |grep 8080 |grep 0.0.0.0
+read -n 1 -p "Press any key to continue..."
 
 systemctl daemon-reload
 systemctl start controller-manager.service
 systemctl enable controller-manager.service
-netstat -natp |grep kube-controlle
-pause 
+systemctl status controller-manager.service
+read -n 1 -p "Press any key to continue..."
